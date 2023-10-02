452b6b5585e72ebbb44fa03cb39c0572415f60db
Crazycurly <sam60317@gmail.com>
2023-10-02 15:47:41 +0800

    First commit

---
+++ homework 1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 47;
+            int width = 22;
+            int area = length * width;
+            Console.WriteLine("number of size in a house : " + area + " m2");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ homework 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace homework_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Celsius to Fahrenheit
+            Console.WriteLine("Celsius to Fahrenheit");
+            double celsius = 32;
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+        }
+    }
+}
